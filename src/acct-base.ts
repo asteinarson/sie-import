8965efdc59495fd805850ae9746dbd26b3364053
Arne Steinarson <asteinarson@gmail.com>
2021-03-01 18:21:48 +0100

    config handling

---
+++ src/acct-base.ts
@@ -0,0 +1,26 @@
+import { getAll, checkTable, upsert } from "./acct-db.js"
+
+let config: Record<string, string> = {}
+
+async function readConfig() {
+    let r = await getAll("config");
+    config = {}
+    r.array.forEach((c: any) => {
+        config[c.key] = c.value
+    });
+}
+
+export async function init() {
+    await readConfig()
+    return true
+}
+
+export function getConfig(key: string, default_val: string): string {
+    v = config[key]
+    if (v != undefined) return v
+    return default_val
+}
+
+export async function setConfig(key: string, value: string) {
+    return upsert("config", { key, value })
+}
