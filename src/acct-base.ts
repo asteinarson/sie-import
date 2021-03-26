47cb102bd45bb08f0270761a574be11928f5c548
Arne Steinarson <asteinarson@gmail.com>
2021-03-26 15:13:07 +0100

    Check tables in init()

--- src/acct-base.ts
+++ src/acct-base.ts
@@ -4,10 +4,20 @@ import { Dict } from "./utils.js"
 let config: Record<string, string> = {}
 
 export async function init() {
-    let r = await checkTable("verification", ["id","description","number"]);
-    if( Array.isArray(r) ){
-        console.log( "init - checkTable, failed: ", r )
-        return false
+    let db_cols: Dict<string[]> = {
+        verification: ["ida", "description", "number", "date"],
+        verification_row: ["credit", "debet", "verification_id", "account_id"],
+        account: ["number", "description", "type", "year"],
+        config: ["key", "value"],
+    }
+    let es:string[]= [];
+    for (let k of Object.keys(db_cols)) {
+        let e = await checkTable(k, db_cols[k]);
+        if (e)  es = es.concat(e);
+    }
+    if( es.length>0 ){
+        console.log("init - checkTable - failed on: \n" + es.toString());
+        return false;
     }
     await readConfig()
     return true
@@ -17,9 +27,9 @@ async function readConfig() {
     let r = await getAll("config");
     config = {}
     r.forEach((c: any) => {
-        config[c.key] = c.value
-    });    
-}    
+        config[c.key] = c.value;
+    });
+}
 
 export function getConfig(key: string, default_val: string): string {
     let v = config[key]
@@ -27,10 +37,10 @@ export function getConfig(key: string, default_val: string): string {
     return default_val
 }
 
-export async function setConfig(key: string, value: string, comment?:string ) {
+export async function setConfig(key: string, value: string, comment?: string) {
     // Optimistic approach
     config[key] = value
-    let vals:Dict<string> = { key, value }
-    if( comment ) vals.comment = comment
+    let vals: Dict<string> = { key, value }
+    if (comment) vals.comment = comment
     return upsert("config", vals, "key")
 }
